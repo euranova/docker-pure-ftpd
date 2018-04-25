b31d0e396e249fa917ed8283755de092a1ee0784
Jehan Bruggeman <jehan.bruggeman@euranova.eu>
2018-04-25 21:52:47 +0200

    add env vars to configure ADVERTISED_HOST and PORT_RANGE

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,5 +19,12 @@ $FTP_PASSWORD
 $FTP_PASSWORD
 EOF
 
+PORT_RANGE=${PORT_RANGE:-30000:31000}
+GUESSED_IP=$(ip addr | grep 'state UP' -A2 | head -n3 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
+ADVERTISED_HOST=${ADVERTISED_HOST:-${GUESSED_IP}}
+
 echo "Running run.sh..."
-./run.sh $@
+./run.sh -c 1000 -C 300 -y 100:100 -l puredb:/etc/pure-ftpd/pureftpd.pdb \
+                    -E -j -R -P ${ADVERTISED_HOST} -p ${PORT_RANGE} \
+                    -s -A -j -Z -H -4 -E -R -G -X -x \
+                    -d -d -O w3c:/var/log/pure-ftpd/transfer.log
